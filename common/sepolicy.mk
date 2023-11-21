dfbdc793fd4d44e82cf5411fc99af57cb3a8a82e
unsatifsed27 <unsatifsederror@gmail.com>
2023-11-21 20:07:24 +0700

    sepolicy: rebrand into eunoia
    Change-Id: I1c72e1d129f78630bfdb30141e7047ac952c7ba0
    Signed-off-by: unsatifsed27 <unsatifsederror@gmail.com>

--- common/sepolicy.mk
+++ common/sepolicy.mk
@@ -1,6 +1,6 @@
 #
 # This policy configuration will be used by all products that
-# inherit from Lineage
+# inherit from Eunoia
 #
 
 ifeq ($(TARGET_COPY_OUT_VENDOR), vendor)
@@ -16,25 +16,25 @@ endif
 endif
 
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += \
-    device/lineage/sepolicy/common/public
+    device/eunoia/sepolicy/common/public
 
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
-    device/lineage/sepolicy/common/private
+    device/eunoia/sepolicy/common/private
 
 ifeq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
-    device/lineage/sepolicy/common/dynamic \
-    device/lineage/sepolicy/common/system
+    device/eunoia/sepolicy/common/dynamic \
+    device/eunoia/sepolicy/common/system
 else
 BOARD_VENDOR_SEPOLICY_DIRS += \
-    device/lineage/sepolicy/common/dynamic \
-    device/lineage/sepolicy/common/vendor
+    device/eunoia/sepolicy/common/dynamic \
+    device/eunoia/sepolicy/common/vendor
 endif
 
 # Selectively include legacy rules defined by the products
--include device/lineage/sepolicy/legacy-common/sepolicy.mk
+-include device/eunoia/sepolicy/legacy-common/sepolicy.mk
 
 # Include atv rules on atv product
 ifeq ($(PRODUCT_IS_ATV), true)
-include device/lineage/sepolicy/atv/sepolicy.mk
+include device/eunoia/sepolicy/atv/sepolicy.mk
 endif
