0a10ceaff779e239cbbdfb7c58e65b179c292fbe
unsatifsed27 <unsatifsederror@gmail.com>
2024-01-08 08:40:29 +0100

    eunoia: rebrand into eunoia
    Change-Id: I2a69d87bd554aeda82231d650555b39400d1aaca
    Signed-off-by: unsatifsed27 <unsatifsederror@gmail.com>

--- atv/sepolicy.mk
+++ atv/sepolicy.mk
@@ -1,12 +1,12 @@
 #
 # This policy configuration will be used by atv products that
-# inherit from Lineage
+# inherit from Eunoia
 #
 
 ifneq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
 BOARD_SEPOLICY_DIRS += \
-    device/lineage/sepolicy/atv/vendor
+    device/eunoia/sepolicy/atv/vendor
 endif
 
 PRODUCT_PRIVATE_SEPOLICY_DIRS += \
-   device/lineage/sepolicy/atv/private
+   device/eunoia/sepolicy/atv/private
